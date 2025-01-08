353d2cb4398e8049d837643207f0033fed8ec233
omerfatihuyar1 <104675284+fatih19uyar@users.noreply.github.com>
2025-01-08 01:27:27 +0100

    Add PostgreSQL connection and user retrieval endpoint using Gin

--- main.go
+++ main.go
@@ -1,7 +1,67 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
+)
+
+// Connection string for PostgreSQL
+const (
+	host     = "localhost"
+	port     = 5433
+	user     = "postgres"
+	password = "password"
+	dbname   = "omerfatihuyar"
+)
 
 func main() {
-	fmt.Println("Merhaba, Dünya!")
+	// Create a new router
+	r := gin.Default()
+
+	// Connect to PostgreSQL
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		log.Fatalf("Unable to connect to database: %v\n", err)
+	}
+	defer db.Close()
+
+	// Test the connection
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("Unable to ping the database: %v\n", err)
+	}
+	log.Println("Successfully connected to the database")
+
+	// Define a simple route
+	r.GET("/users", func(c *gin.Context) {
+		var users []string
+		rows, err := db.Query("SELECT username FROM users")
+		if err != nil {
+			log.Fatalf("Unable to execute query: %v\n", err)
+		}
+		defer rows.Close()
+
+		for rows.Next() {
+			var username string
+			err = rows.Scan(&username)
+			if err != nil {
+				log.Fatalf("Unable to scan row: %v\n", err)
+			}
+			users = append(users, username)
+		}
+
+		c.JSON(http.StatusOK, gin.H{"users": users})
+	})
+
+	// Run the server
+	r.Run(":8080")
 }
